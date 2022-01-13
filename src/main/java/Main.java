b605901b650b4d8e86fd842097675d968bdd63b4
Marek Domanski <markoneti@poczta.onet.pl>
2022-01-13 11:48:57 +0100

    first modification of Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hej");
+        System.out.println("Helo World");
     }
     private void displayText() {
         System.out.println("Change on Raspberry")
