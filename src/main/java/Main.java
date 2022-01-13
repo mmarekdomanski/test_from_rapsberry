c10655cfa84c6cbd5faa48c78a9ecd8eef20fd75
Raspberry <dyrdymal@poczta.wp.pl>
2022-01-13 13:18:47 +0100

    changed second time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args){
+
         System.out.println("Helo Mariusz");
-    }
+
     private void displayText() {
         System.out.println("Change on Raspberry");
     }
