d63f321de816583465b05f3a93f8ab5e3eb9325a
Marek Domanski <markoneti@poczta.onet.pl>
2022-01-13 13:11:36 +0100

    channge Mariusz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Helo World");
+        System.out.println("Helo Mariusz");
     }
     private void displayText() {
-        System.out.println("Change on Raspberry")
+        System.out.println("Change on Raspberry");
     }
 }
