fd8cf12ed5f2f3b3ee3f76cf42613bf2a527b6ae
Raspberry <dyrdymal@poczta.wp.pl>
2022-01-13 11:53:46 +0100

    Changes on Raspberry

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hej");
     }
+    private void displayText() {
+        System.out.println("Change on Raspberry")
+    }
 }
