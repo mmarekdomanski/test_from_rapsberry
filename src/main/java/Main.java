866556ec71e917c8933a19ebfa0145c8beeba41b
Raspberry <dyrdymal@poczta.wp.pl>
2022-01-13 13:39:33 +0100

    final resolving

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args){
+
         System.out.println("Helo Dominik i Mariusz");
-    }
+
     private void displayText() {
         System.out.println("Change on Raspberry");
     }
